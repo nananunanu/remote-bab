8c4e60f3494c03635f35e396b62fa4b05ebff872
0o0o0o1 <rlarjsdn0217@naver.com>
2024-06-18 19:14:35 +0900

    first page

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import './action.js';
+import { motion } from "framer-motion";
+
+import gif from './image/Bean Eater@4x-1.0s-200px-200px.gif';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="screen">
+        
+     
+        <div>
+          
+          <p className='lunch'>
+          
+            점메추<br></br>
+          </p> 
+          <p>
+          점심 메뉴를 추천<br></br>
+          해 줄래요?
+          
+          
+        </p>
+        <motion.img 
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+         className='gif' src={gif}/>
+        </div>
+        
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
+
+// $function() {
+//   function
+// }
+
+
+
+// .btn:hover {
+//   background-color:#002ead;
+//   transition: 0.7s;
+// }
---
+++ src/action.js
@@ -0,0 +1,16 @@
+{/* <img      
+      src='icon.jpg'
+      onMouseOver={(event) =>{ this.handlerOver(event);}}
+      onMouseOut={(event) => { this.handlerOut(event); }}
+/> */}
+
+// $function() {
+//   function
+// }
+
+
+
+
+//   background-color:#002ead;
+//   transition: 0.7s;
+// }
